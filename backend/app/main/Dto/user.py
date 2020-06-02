dc52caf50f1e1d62495e9005d6eb931d5497ffa3
tcphat <tcphat@tma.com.vn>
2020-06-02 15:41:51 +0700

    test-deploy-node-heroku

--- backend/app/main/Dto/user.py
+++
@@ -1,9 +0,0 @@
-from flask_restplus import Namespace, fields
-
-class UserDto:
-    api = Namespace('user', description='user related operations')\
-
-    create_normal_user = api.model('create_user', {
-        'user_name': fields.String(required = True),
-        'password': fields.String(required = True),
-    })
